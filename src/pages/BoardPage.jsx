0f998213e5d9038d407d1f1d7b26b44a3837df13
LeeSooBin <romaydev@naver.com>
2024-06-25 13:55:38 +0900

    Feat : 게시판 게시글 보기 기능 구현 게시글 상세페이지 작업해야함.

--- src/pages/BoardPage.jsx
+++ src/pages/BoardPage.jsx
@@ -1,11 +1,24 @@
 import styles from "./BoardPage.module.css";
 import BoardTitle from "../board/BoardTitle";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const BoardPage = () => {
+  const [boardData, setBoardData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      axios("http://localhost:8080/board/home").then((result) => {
+        setBoardData(result.data);
+      });
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className={styles.container}>
-      {response.data.map((data) => (
-        <BoardTitle boardId={data.id} userId={data.userId} title={data.title} date={data.date} view={data.view} />
+      {boardData.map((data) => (
+        <BoardTitle boardId={data.boardCode} key={data.boardCode} title={data.boardTitle} userId={data.userNickName} view={data.boardView} date={data.boardDate} />
       ))}
     </div>
   );
@@ -13,42 +26,42 @@ const BoardPage = () => {
 
 export default BoardPage;
 
-const response = {
-  data: [
-    {
-      id: "1",
-      title: "가나다라마바사 테스트 글제목",
-      userId: "romay99",
-      date: "2020-22-22",
-      view: "999",
-    },
-    {
-      id: "2",
-      title: "가나다라마바사 테스트 글제목",
-      userId: "romay99",
-      date: "2020-22-22",
-      view: "125",
-    },
-    {
-      id: "3",
-      title: "가나다라마바사 테스트 글제목",
-      userId: "romay99",
-      date: "2020-22-22",
-      view: "111",
-    },
-    {
-      id: "4",
-      title: "가나다라마바사 테스트 글제목",
-      userId: "romay99",
-      date: "2020-22-22",
-      view: "1124521",
-    },
-    {
-      id: "5",
-      title: "가나다라마바사 테스트 글제목",
-      userId: "romay99",
-      date: "2020-22-22",
-      view: "12511",
-    },
-  ],
-};
+// const response = {
+//   data: [
+//     {
+//       id: "1",
+//       title: "가나다라마바사 테스트 글제목",
+//       userId: "romay99",
+//       date: "2020-22-22",
+//       view: "999",
+//     },
+//     {
+//       id: "2",
+//       title: "가나다라마바사 테스트 글제목",
+//       userId: "romay99",
+//       date: "2020-22-22",
+//       view: "125",
+//     },
+//     {
+//       id: "3",
+//       title: "가나다라마바사 테스트 글제목",
+//       userId: "romay99",
+//       date: "2020-22-22",
+//       view: "111",
+//     },
+//     {
+//       id: "4",
+//       title: "가나다라마바사 테스트 글제목",
+//       userId: "romay99",
+//       date: "2020-22-22",
+//       view: "1124521",
+//     },
+//     {
+//       id: "5",
+//       title: "가나다라마바사 테스트 글제목",
+//       userId: "romay99",
+//       date: "2020-22-22",
+//       view: "12511",
+//     },
+//   ],
+// };
